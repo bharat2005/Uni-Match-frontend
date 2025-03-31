b9e61b8c7fb3d8e8ad7f208f84761f2ca665cc7a
bharat2005 <bharatvdeshm2005@gmail.com>
2025-03-31 17:38:52 +0530

    web

---
+++ match/src/components/dashboard/Chatoo2.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://api.uni-match.in:5001"); // Change this to your server IP
+
+function App() {
+  useEffect(() => {
+    // When connected
+    socket.on("connect", () => {
+      console.log("Connected to WebSocket server!");
+      socket.send("Hello from React!"); // Send message when connected
+    });
+
+    // When receiving a message
+    socket.on("message", (data) => {
+      console.log("Received:", data);
+    });
+
+    return () => {
+      socket.disconnect(); // Cleanup on unmount
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>WebSocket Test</h1>
+      <button onClick={() => socket.send("Hello Flask!")}>
+        Send Message
+      </button>
+    </div>
+  );
+}
+
+export default App;
