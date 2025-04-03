59fa3851205d5070d244a221c8868cd1399a0f17
bharat2005 <bharatvdeshm2005@gmail.com>
2025-04-03 10:21:23 +0530

    avatar

--- match/src/components/dashboard/Chatsoo.js
+++ match/src/components/dashboard/Chatsoo.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../AuthProvider";
 import axios from "axios";
 import { db } from './firebase';
-import { collection, writeBatch ,doc, serverTimestamp, getDocs, orderBy, where, query, onSnapshot, limit, writeBatch } from "firebase/firestore";
+import { collection ,doc, serverTimestamp, getDocs, orderBy, where, query, onSnapshot, limit, writeBatch } from "firebase/firestore";
 import {
   Avatar,
   Badge,
